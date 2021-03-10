92718056d0b6125ef903db73f55d051c768b690e
Demontisa <49113774+Demontisa@users.noreply.github.com>
2021-03-10 12:58:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,73 @@
-# coding: utf-8
-'''
-@author: sy-records
-@license: https://github.com/sy-records/v-checkin/blob/master/LICENSE
-@contact: 52o@qq52o.cn
-@desc: 腾讯视频好莱坞会员V力值签到，支持两次签到：一次正常签到，一次手机签到。
-@blog: https://qq52o.me
-'''
-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
 import requests
+import os
 
-auth_refresh_url = ''
-sckey = ''
+key = os.environ["KEY"]
+# sckey = ''
+send_url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + key
 
-ftqq_url = "https://sc.ftqq.com/%s.send"%(sckey)
-url1 = 'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=hierarchical_task_system&cmd=2'
-url2 = 'https://v.qq.com/x/bu/mobile_checkin'
+# https://access.video.qq.com/user/auth_refresh 获取 cookie
+login_cookie = os.environ["LOGIN_COOKIE"]
+signin_cookie = os.environ["SIGNIN_COOKIE"]
+auth_refresh_url = os.environ["AUTH_REFRESH_URL"]
 
-login_headers = {
-    'Referer': 'https://v.qq.com',
-    'Cookie': 'tvfe_boss_uuid=********; pgv_pvid=********; video_guid=***********; video_platform=2; pgv_info=ssid=***********; pgv_pvi=*************; pgv_si=*************; _qpsvr_localtk=***************; ptisp=; ptui_loginuin=************; RK=*************; ptcz=***************; main_login=qq; vqq_access_token=****************; vqq_appid=101483052; vqq_openid=********************; vqq_vuserid=*********************; vqq_vusession=dzsfo; vqq_refresh_token=*****************; uid=**************;'
+Referer = 'https://v.qq.com'
+Agent = 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+headers_login = {
+  'User-Agent': Agent,
+  'Cookie': login_cookie,
+  'Referer': Referer
 }
 
-login = requests.get(auth_refresh_url, headers=login_headers)
+# 测试一个签到请求
+login = requests.get(auth_refresh_url, headers=headers_login)
 cookie = requests.utils.dict_from_cookiejar(login.cookies)
-
+# 如果请求返回信息包含no login说明cookie已经失效
 if not cookie:
-    print "auth_refresh error"
-    payload = {'text': '腾讯视频V力值签到通知', 'desp': '获取Cookie失败，Cookie失效'}
-    requests.post(ftqq_url, params=payload)
+    print("auth_refresh error")
+    params = {'text': '腾讯视频V力值签到通知', 'desp': '获取Cookie失败，Cookie失效'}
+    requests.post(send_url, params=params)
+urls = [
+    'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=spp_MissionFaHuo&cmd=4&task_id=7&_=1582364733058&callback=Zepto1582364712694',
+    # 下载签到请求
+    'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=spp_MissionFaHuo&cmd=4&task_id=6&_=1582366326994&callback=Zepto1582366310545',
+    # 赠送签到请求
+    'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=hierarchical_task_system&cmd=2&_=1555060502385&callback=Zepto1555060502385',
+    # 签到请求
+    'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=spp_MissionFaHuo&cmd=4&task_id=3&_=1582368319252&callback=Zepto1582368297765 ',
+    # 弹幕签到请求
+    'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=spp_MissionFaHuo&cmd=4&task_id=1&_=1582997048625&callback=Zepto1582997031843',
+    # 观看60分钟签到
+]
+count = 0
+resultContent = ''
+for url in urls:
+    count += 1
+    if (count == 1):
+        print("发送每日下载任务请求")
+    elif (count == 2):
+        print("发送每日赠片任务请求")
+    elif (count == 3):
+        print("发送每日签到任务请求")
+    elif (count == 4):
+        print("发送每日弹幕任务请求")
+    elif (count == 5):
+        print("发送每日观影60分钟任务请求")
+    refresh_cookie = cookie['vqq_vusession']
+    headers_signin = {
+      'User-Agent': Agent,
+      'Cookie': signin_cookie + refresh_cookie + ';vqq_vusession=' + refresh_cookie + ';',
+      'Referer': Referer
+    }
+    response = requests.get(url=url, headers=headers_signin)
+    responseContent = response.content.decode("utf-8")
+    print(responseContent)
+    resultContent += responseContent + '\n\n'
 
-sign_headers = {
-    'Cookie': 'tvfe_boss_uuid=***********; pgv_pvid=***************; video_guid=***************; video_platform=2; pgv_info=ssid=****************; pgv_pvi=****************; pgv_si=***************; _qpsvr_localtk=*************; ptisp=; ptui_loginuin=***************; RK=****************; ptcz=*********************; main_login=qq; vqq_access_token=************; vqq_appid=101483052; vqq_openid=*************; vqq_vuserid=*************; vqq_vusession=' + cookie['vqq_vusession'] + ';',
-    'Referer': 'https://m.v.qq.com'
+params = {
+    "msgtype": "markdown",
+    "markdown": {
+        "content": "<font color=\"warning\">腾讯视频签到通知</font>\n" + resultContent
+    }
 }
-def start():
-  sign1 = requests.get(url1,headers=sign_headers).text
-  if 'Account Verify Error' in sign1:
-    print 'Sign1 error,Cookie Invalid'
-    status = "链接1 失败，Cookie失效"
-  else:
-    print 'Sign1 Success'
-    status = "链接1 成功，获得V力值：" + sign1[42:-14]
-
-  sign2 = requests.get(url2,headers=sign_headers).text
-  if 'Unauthorized' in sign2:
-    print 'Sign2 error,Cookie Invalid'
-    status = status + "\n\n 链接2 失败，Cookie失效"
-  else:
-    print 'Sign2 Success'
-    status = status + "\n\n 链接2 成功"
-
-  payload = {'text': '腾讯视频V力值签到通知', 'desp': status}
-  requests.post(ftqq_url, params=payload)
-
-def main_handler(event, context):
-  return start()
-if __name__ == '__main__':
-  start()
+requests.post(send_url, params=params)
+print('已通知到企业微信')
